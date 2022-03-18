68fa993f4812e779d55c2a5f142efa577f55c55d
LuisGarciaDev <garciarinconluis@gmail.com>
2022-03-18 18:53:02 -0500

    pagina Perfil

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../container/Home";
 import Home2 from "../container/Home2";
 import Login from "../container/Login";
+import Perfil from "../container/Perfil";
 import Register from "../container/Register";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<Home2 />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   );
