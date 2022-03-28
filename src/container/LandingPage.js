148579a6d385286e7004be63d563cc49be8ed087
Daniel Gomez <danielrozo11.49@gmail.com>
2022-03-28 11:24:42 -0500

    Cambios en el landing page

--- src/container/LandingPage.js
+++ src/container/LandingPage.js
@@ -9,36 +9,42 @@ const LandingPage = () => {
                     <div class="col-lg-7 text-center text-lg-start">
                         <h1 class="display-4 fw-bold lh-1 mb-3">Aumenta tus ingresos</h1>
                         <p class="col-lg-10 fs-4">Digitaliza tu negocio y atiende más clientes en todo el mundo.
-                        Con Mentor's, la plataforma para llevar tu negocio freelance a internet en pocos pasos y sin complicaciones</p>
+                            Con Mentor's, la plataforma para llevar tu negocio freelance a internet en pocos pasos y sin complicaciones</p>
                         <div class="d-grid gap-1 text-center">
                             <button class="btn btn-success w-50" type="button">Registrarse</button>
                         </div>
                     </div>
                     <div class="col-md-10 mx-auto col-lg-5">
-                        <img className='w-100' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648349074/Digital_presentation-pana_hzaqzg.png' />
+                        <img className='w-100' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648349074/Digital_presentation-pana_hzaqzg.png' alt="mentoria" />
                     </div>
                 </div>
             </div>
 
             <div className='container row mx-5'>
                 <div class=" col card border-success mx-5">
-                    <div class="card-header bg-transparent border-success text-center"><img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648482956/proyecto-final/Video_call-bro_vsidud.png'></img></div>
+                    <div class="card-header bg-transparent border-success text-center">
+                        <img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648482956/proyecto-final/Video_call-bro_vsidud.png' alt="card" />
+                    </div>
                     <div class="card-body text-success">
-                        <h5 class="card-title">Mentorías personalizadas</h5>
+                        <h5 class="card-title text-black">Mentorías personalizadas</h5>
                         <p class="card-text">Puedes tener sesiones 1 a 1 con founders expertos que resolverán tus dudas e impulsarán el crecimiento de tu Startup.</p>
                     </div>
                 </div>
                 <div class="col card border-success mx-5">
-                    <div class="card-header bg-transparent border-success text-center"><img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648483017/proyecto-final/Online_learning-pana_qlubim.png'></img></div>
+                    <div class="card-header bg-transparent border-success text-center">
+                        <img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648483017/proyecto-final/Online_learning-pana_qlubim.png' alt="card" />
+                    </div>
                     <div class="card-body text-success">
-                        <h5 class="card-title">Paga por sesión</h5>
+                        <h5 class="card-title text-black">Paga por sesión</h5>
                         <p class="card-text">Paga únicamente por sesiones individuales, sin contratos ni suscripciones.</p>
                     </div>
                 </div>
                 <div class="col card border-success mx-5">
-                    <div class="card-header bg-transparent border-success text-center"><img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648483058/proyecto-final/Online_world-amico_iba9hm.png'></img></div>
+                    <div class="card-header bg-transparent border-success text-center">
+                        <img className='w-50' src='https://res.cloudinary.com/dmdu8u7wo/image/upload/v1648483058/proyecto-final/Online_world-amico_iba9hm.png' alt="card" />
+                    </div>
                     <div class="card-body text-success">
-                        <h5 class="card-title">Dónde quieras, cuándo quieras</h5>
+                        <h5 class="card-title text-black">Dónde quieras, cuándo quieras</h5>
                         <p class="card-text">Recibe mentoría de founders expertos desde tu oficina, casa o donde sea que estés.</p>
                     </div>
                 </div>
@@ -72,7 +78,7 @@ const LandingPage = () => {
                     </div>
                 </div>
             </div>
-<Footer/>
+            <Footer />
         </div>
     )
 }
